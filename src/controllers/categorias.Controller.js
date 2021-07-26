20ae30ce0182b1581d471451e4e6eade0dbcafaf
Pier <pedrolosasp@gmail.com>
2021-07-26 18:08:35 +0200

    controllers_categorias

---
+++ src/controllers/categorias.Controller.js
@@ -0,0 +1,33 @@
+const db = require('../config/db');
+const helper = require('../helper/helpers');
+
+
+exports.getCategorias = async(req,res) => {
+	const recibido = req.query;
+
+	// validamos que todos los campos que recibe la API  sean los correctos
+	const array_json_validator = helper.require_data(['ID'],recibido );
+
+	if(array_json_validator === false){
+		res.status(401).json({status:false, data:'Faltan datos obligatorios'});
+		return;
+	}
+
+	const result = await db
+						.select('*')
+						.from('categorias')
+						.where("ID" , recibido.ID)
+
+	res.json({status: true, data: result})
+	
+};
+
+exports.getListCategorias = async(req,res) => {
+	const recibido = req.body;
+	const result = await db
+						.select('*')
+						.from('categorias')
+
+	res.json({status: true, data: result})
+	
+};
--- src/routers/categorias.Routes.js
+++ src/routers/categorias.Routes.js
@@ -1,42 +1,17 @@
 const express = require('express');
 const authToken = require('../auth/authtoken');
 const categorias = express.Router();
-const db = require('../config/db');
-const helper = require('../helper/helpers');
+const categoriasController = require('../controllers/categorias.Controller');
 
 
 
 // GET_list
-categorias.get('/get_list',async(req,res) => {
-	const recibido = req.body;
-	const result = await db
-						.select('*')
-						.from('categorias')
-
-	res.json({status: true, data: result})
-	
-});
+categorias.get('/get_list', categoriasController.getListCategorias);
 
 
 // GET
-categorias.get('/get', async(req,res) => {
-	const recibido = req.query;
+categorias.get('/get', categoriasController.getCategorias);
 
-	// validamos que todos los campos que recibe la API  sean los correctos
-	const array_json_validator = helper.require_data(['ID'],recibido );
 
-	if(array_json_validator == false){
-		res.status(401).json({status:false, data:'Faltan datos obligatorios'});
-		return;
-	}
-
-	const result = await db
-						.select('*')
-						.from('categorias')
-						.where("ID" , recibido.ID)
-
-	res.json({status: true, data: result})
-	
-});
 
 module.exports = categorias;
